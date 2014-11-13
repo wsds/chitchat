af36dc23fbceb463f6d0a395cebfb44d815ee39c
wjjm555 <139040491@qq.com>
2014-11-13 13:32:52 +0800

    删除服务器代码

--- microcommune/web/microCommune/setting.js
+++
@@ -1,7 +0,0 @@
-//set window globa data
-window.globaldata = {};
-
-var serverSetting = {};
-globaldata.serverSetting = serverSetting;
-serverSetting.maxUploadImg = "1048576";
-globaldata.tempData = 0;
--- microcommune/webmc/static/js/setting.js
+++
@@ -1,8 +0,0 @@
-//set window globa data
-window.globaldata = {};
-
-var serverSetting = {};
-globaldata.serverSetting = serverSetting;
-serverSetting.maxUploadImg = "1048576";
-serverSetting.imageServer = "http://images1.lejoying.com//";
-globaldata.tempData = 0;
